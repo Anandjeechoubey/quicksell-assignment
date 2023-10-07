4de8d0b1117b37ab08c53a8e87a9397a845681c1
Anand Jee Choubey <anandjechoubey@gmail.com>
2023-10-07 16:04:55 +0530

    Add footer

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {
   ProgressLevels,
   // GroupingOptions
 } from "./constants";
+import Footer from "./components/Footer";
 
 function App() {
   // const [tickets, setTickets] = useState([]);
@@ -112,6 +113,7 @@ function App() {
           );
         })}
       </main>
+      <Footer />
     </body>
   );
 }
